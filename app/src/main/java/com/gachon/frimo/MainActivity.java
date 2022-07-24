e470f46e497c77c24e4ab3df7c9b0a71f1f08dfe
Hongsi-Taste <kwonwk123@gmail.com>
2022-07-25 03:50:54 +0900

    floating menu&button added to mainpage

--- app/src/main/java/com/gachon/frimo/MainActivity.java
+++ app/src/main/java/com/gachon/frimo/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Floating Action Button
+        FloatingActionsMenu fam = (FloatingActionsMenu) findViewById(R.id.floating_menu);
     }
 }
