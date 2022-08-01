20ab557559896b726c97e46c60a98ac4f78b2a55
namsh1125 <namsh1125@naver.com>
2022-08-02 02:47:17 +0900

    Add menu icon in Friendly communtiy and set Kotlin

---
+++ app/src/main/java/com/gachon/frimo/ChangeProfile.kt
@@ -0,0 +1,12 @@
+package com.gachon.frimo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChangeProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
