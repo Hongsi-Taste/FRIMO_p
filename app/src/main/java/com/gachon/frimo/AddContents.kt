f6986a6275ade78eb6e41dd42bdae2d6c1c4e916
namsh1125 <namsh1125@naver.com>
2022-08-03 17:05:28 +0900

    Implement add contents

--- app/src/main/java/com/gachon/frimo/AddContents.kt
+++ app/src/main/java/com/gachon/frimo/AddContents.kt
@@ -1,12 +1,26 @@
 package com.gachon.frimo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class AddContents : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_contents)
+
+        // 기존 action bar 제거 후 custom action bar 넣기
+        supportActionBar!!.hide()
+
+        // menu
+        val icon = findViewById<ImageView>(R.id.ic_menu)
+        icon.setImageResource(R.drawable.ic_upload)
+
+        icon.setOnClickListener {
+            val intent = Intent(this, FriendlyCommunity::class.java)
+            startActivity(intent)
+        }
     }
 }
