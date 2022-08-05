09fd8f946fa84d624b0ae64f3be2c79e3b757bf4
namsh1125 <namsh1125@naver.com>
2022-08-06 00:47:27 +0900

    Code refactoring

--- app/src/main/java/com/gachon/frimo/Login.kt
+++ app/src/main/java/com/gachon/frimo/Login.kt
@@ -2,33 +2,32 @@ package com.gachon.frimo
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.gachon.frimo.databinding.LoginBinding
 
 class Login : AppCompatActivity() {
 
+    private lateinit var binding: LoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
 
-        var button_login: Button = findViewById(R.id.button_login)
-        var button_signUp: Button = findViewById(R.id.button_signUp)
-        var button_findIdPw: Button = findViewById(R.id.button_findIdPw)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button_login.setOnClickListener {
+        binding.buttonLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        button_signUp.setOnClickListener {
+        binding.buttonSignUp.setOnClickListener {
             val intent = Intent(this, TermsOfUse::class.java)
             startActivity(intent)
         }
 
-        button_findIdPw.setOnClickListener{
+        binding.buttonFindIdPw.setOnClickListener {
             val intent = Intent(this, FindId::class.java)
             startActivity(intent)
         }
