855f35db69b07b1f95273f8ef32752d38e39b283
namsh1125 <namsh1125@naver.com>
2022-08-11 14:00:39 +0900

    Implement friendly community interest

--- app/src/main/java/com/gachon/frimo/FriendlyCommunity.kt
+++ app/src/main/java/com/gachon/frimo/FriendlyCommunity.kt
@@ -7,7 +7,10 @@ import android.view.Gravity
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.gachon.frimo.adapter.InterestAdapter
 import com.gachon.frimo.databinding.FriendlyCommunityBinding
+import com.gachon.frimo.entity.Interest
 import com.skydoves.powermenu.MenuAnimation
 import com.skydoves.powermenu.OnMenuItemClickListener
 import com.skydoves.powermenu.PowerMenu
@@ -17,6 +20,7 @@ class FriendlyCommunity : AppCompatActivity() {
 
     private lateinit var menu : PowerMenu
     private lateinit var binding: FriendlyCommunityBinding
+    private val interest: ArrayList<Interest> = ArrayList() // Interests
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -27,6 +31,15 @@ class FriendlyCommunity : AppCompatActivity() {
 
         // 기존 action bar 제거 후 custom action bar 넣기
         supportActionBar!!.hide()
+
+        binding.labelInterests.setOnClickListener{
+
+            prepareInterestData(); // RecyclerView에 보여 줄 data 받아오기
+            binding.recyclerView.adapter = InterestAdapter(interest)
+            binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+        }
+
+
 
         // menu
         val icon = findViewById<ImageView>(R.id.ic_menu)
@@ -72,4 +85,20 @@ class FriendlyCommunity : AppCompatActivity() {
             }
         }
 
+    private fun prepareInterestData() {
+
+        // Todo: 크롤링으로 data 받아오기
+        interest.clear()
+        interest.add(Interest(R.drawable.img_sample, "test1"))
+        interest.add(Interest(R.drawable.img_sample, "test2"))
+        interest.add(Interest(R.drawable.img_sample, "test3"))
+        interest.add(Interest(R.drawable.img_sample, "test4"))
+        interest.add(Interest(R.drawable.img_sample, "test5"))
+        interest.add(Interest(R.drawable.img_sample, "test6"))
+        interest.add(Interest(R.drawable.img_sample, "test7"))
+        interest.add(Interest(R.drawable.img_sample, "test8"))
+        interest.add(Interest(R.drawable.img_sample, "test9"))
+        interest.add(Interest(R.drawable.img_sample, "test10"))
+
+    }
 }
