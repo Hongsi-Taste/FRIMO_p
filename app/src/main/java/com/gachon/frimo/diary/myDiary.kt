7c2f2f78d638def516d98ca91ae63bf2d8cb87e8
Hongsi-Taste <kwonwk123@gmail.com>
2022-08-19 05:11:23 +0900

    diary filling

--- app/src/main/java/com/gachon/frimo/diary/myDiary.kt
+++ app/src/main/java/com/gachon/frimo/diary/myDiary.kt
@@ -1,16 +1,22 @@
 package com.gachon.frimo.diary
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.view.Gravity
 import android.widget.Toast
 import com.gachon.frimo.databinding.ActivityMyDiaryBinding
 import com.gachon.frimo.databinding.FragmentMyDiaryChatBinding
 import com.gachon.frimo.databinding.FragmentMyDiaryGalleryBinding
 import com.gachon.frimo.databinding.FragmentMyDiarySecretBinding
+import com.skydoves.powermenu.MenuAnimation
+import com.skydoves.powermenu.OnMenuItemClickListener
+import com.skydoves.powermenu.PowerMenu
+import com.skydoves.powermenu.PowerMenuItem
 
 //TODO: add powermenu
 class MyDiary : AppCompatActivity() {
@@ -21,13 +27,28 @@ class MyDiary : AppCompatActivity() {
     private lateinit var bindingSecret: FragmentMyDiarySecretBinding
     private lateinit var bindingGallery: FragmentMyDiaryGalleryBinding
 
+    //PowerMenu
+    private lateinit var menu: PowerMenu
+
     //필요한 클래스 미리 불러놓기
     private lateinit var speechRecognizer: SpeechRecognizer
     private lateinit var recognitionListener: RecognitionListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+
+        binding = ActivityMyDiaryBinding.inflate(layoutInflater)
+
+        //Powermenu
+        menu = PowerMenu.Builder(this)
+            .addItem(PowerMenuItem("Diary",false))
+            .addItem(PowerMenuItem("Secret",false))
+            .addItem(PowerMenuItem("Gallery",false))
+            .setAnimation(MenuAnimation.SHOWUP_TOP_RIGHT)
+            .setTextColor(Color.BLACK)
+            .setTextGravity(Gravity.CENTER)
+            .setOnMenuItemClickListener(changeMenuItemClickListener)
+            .build()
 
         //TODO: add permission check and of not satisfied, request permission
         //intent에 Recognizer넣어서 선언.
@@ -112,5 +133,16 @@ class MyDiary : AppCompatActivity() {
             }
 
         }
+    }
+
+    //PowerMenu Item Click Listener
+    private val changeMenuItemClickListener =
+        OnMenuItemClickListener<PowerMenuItem> { position, item ->
+
+//            when(position){
+//                0 ->
+//                1 ->
+//                2 -> s
+//            }
     }
 }
