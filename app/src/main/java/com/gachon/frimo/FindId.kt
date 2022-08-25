ac6f57748bf9e508390d4a76be88f550ae5891b4
namsh1125 <namsh1125@naver.com>
2022-08-26 03:10:34 +0900

    delete powermenu in

--- app/src/main/java/com/gachon/frimo/FindId.kt
+++ app/src/main/java/com/gachon/frimo/FindId.kt
@@ -1,21 +1,12 @@
 package com.gachon.frimo
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gachon.frimo.databinding.FindIdBinding
-import com.skydoves.powermenu.MenuAnimation
-import com.skydoves.powermenu.OnMenuItemClickListener
-import com.skydoves.powermenu.PowerMenu
-import com.skydoves.powermenu.PowerMenuItem
 
 class FindId : AppCompatActivity() {
 
-    private lateinit var menu: PowerMenu
     private lateinit var binding: FindIdBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,43 +16,21 @@ class FindId : AppCompatActivity() {
         binding = FindIdBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 기존 action bar 제거 후 custom action bar 넣기
-        supportActionBar!!.hide()
-
-        // set menu icon
-        val icon = findViewById<ImageView>(R.id.ic_menu)
-        icon.setImageResource(R.drawable.ic_list)
-
-        // menu icon click listener
-        icon.setOnClickListener { view ->
-            menu.showAsDropDown(view)
-        }
-
-        menu = PowerMenu.Builder(this)
-            .addItem(PowerMenuItem("Find Id", true))
-            .addItem(PowerMenuItem("Find Pw", false))
-            .setAnimation(MenuAnimation.SHOWUP_TOP_RIGHT) // popup시 menu가 뜨는 위치
-            .setTextColor(Color.BLACK)
-            .setTextGravity(Gravity.CENTER) // 글자 위치
-            .setSelectedMenuColor(getResources().getColor(R.color.purple_200))
-            .setSelectedTextColor(Color.WHITE)
-            .setOnMenuItemClickListener(changeMenuItemClickListener)
-            .build()
+        // Title 변경
+        supportActionBar!!.setTitle("아이디 찾기")
 
         binding.buttonFindId.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+
+            // Todo: ID 찾아주기
+
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
+
+        binding.buttonFindPw.setOnClickListener {
+            startActivity(Intent(this, FindPw::class.java))
             finish()
         }
 
     }
-
-    // change menu item click listener
-    private val changeMenuItemClickListener =
-        OnMenuItemClickListener<PowerMenuItem> { position, item ->
-
-            when (position) {
-                1 -> startActivity(Intent(this, FindPw::class.java))
-            }
-        }
 }
--- app/src/main/java/com/gachon/frimo/FindPw.kt
+++ app/src/main/java/com/gachon/frimo/FindPw.kt
@@ -25,43 +25,21 @@ class FindPw : AppCompatActivity() {
         binding = FindPwBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 기존 action bar 제거 후 custom action bar 넣기
-        supportActionBar!!.hide()
-
-        // set menu icon
-        val icon = findViewById<ImageView>(R.id.ic_menu)
-        icon.setImageResource(R.drawable.ic_list)
-
-        // menu icon click listener
-        icon.setOnClickListener { view ->
-            menu.showAsDropDown(view)
-        }
-
-        menu = PowerMenu.Builder(this)
-            .addItem(PowerMenuItem("Find Id", false))
-            .addItem(PowerMenuItem("Find Pw", true))
-            .setAnimation(MenuAnimation.SHOWUP_TOP_RIGHT) // popup시 menu가 뜨는 위치
-            .setTextColor(Color.BLACK)
-            .setTextGravity(Gravity.CENTER) // 글자 위치
-            .setSelectedMenuColor(getResources().getColor(R.color.purple_200))
-            .setSelectedTextColor(Color.WHITE)
-            .setOnMenuItemClickListener(changeMenuItemClickListener)
-            .build()
+        // Title
+        supportActionBar!!.setTitle("비밀번호 찾기")
 
         binding.buttonFindPw.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+
+            // Todo: PW 찾아주기
+
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
+
+        binding.buttonFindId.setOnClickListener {
+            startActivity(Intent(this, FindId::class.java))
             finish()
         }
 
     }
-
-    // change menu item click listener
-    private val changeMenuItemClickListener =
-        OnMenuItemClickListener<PowerMenuItem> { position, item ->
-
-            when (position) {
-                0 -> startActivity(Intent(this, FindId::class.java))
-            }
-        }
 }
