5905f48d75c9853ed4121ea032f30c577d552c8d
namsh1125 <namsh1125@naver.com>
2022-08-27 03:16:42 +0900

    Change inherited activity

--- app/src/main/java/com/gachon/frimo/Login.kt
+++ app/src/main/java/com/gachon/frimo/Login.kt
@@ -2,10 +2,10 @@ package com.gachon.frimo
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import com.gachon.frimo.databinding.LoginBinding
 
-class Login : AppCompatActivity() {
+class Login : ComponentActivity() {
 
     private lateinit var binding: LoginBinding
 
